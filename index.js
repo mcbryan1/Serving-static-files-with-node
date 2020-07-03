aa2ee092fcd5d0de97eea7e0e121f4381ea4f5a3
McBryan <solomonmcbryan@gmail.com>
2020-07-03 20:33:52 +0000

    Manual Approach

--- index.js
+++ index.js
@@ -7,8 +7,34 @@ const server = express();
 server.use(express.static(path.join(__dirname, "public")));
 
 //Request Handling
+const homePage =(req, res)=>{
+    const homePagePath = path.join(__dirname, 'public', 'index.html')
+    res.sendFile(homePagePath)
+}
+
+const aboutPage =(req, res)=>{
+    const aboutPagePath = path.join(__dirname, 'public', 'about.html')
+    res.sendFile(aboutPagePath)
+}
+const portfolioPage =(req, res)=>{
+    const portfolioPagePath = path.join(__dirname, 'public', 'portfolio.html')
+    res.sendFile(portfolioPagePath)
+}
+const careerPage =(req, res)=>{
+    const careerPagePath = path.join(__dirname, 'public', 'career.html')
+    res.sendFile(careerPagePath)
+}
+const contactPage =(req, res)=>{
+    const contactPagePath = path.join(__dirname, 'public', 'contact.html')
+    res.sendFile(contactPagePath)
+}
 
 //Routes
+server.get('/', homePage)
+server.get('/about', aboutPage)
+server.get('/portfolio', portfolioPage)
+server.get('/career', careerPage)
+server.get('/contact', contactPage)
 
 //Listening To Server
 server.listen(3001, () => console.log("Server Is Ready For Any Request"));
